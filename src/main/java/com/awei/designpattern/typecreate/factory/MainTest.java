1740aa961ccb964b1badaad35fbc99ab433ec26f
awei <awei4home@163.com>
2019-03-12 18:44:42 +0800

    工厂模式

--- src/main/java/com/awei/designpattern/typecreate/factory/MainTest.java
+++ src/main/java/com/awei/designpattern/typecreate/factory/MainTest.java
@@ -2,12 +2,14 @@ package com.awei.designpattern.typecreate.factory;
 
 public class MainTest {
     public static void main(String[] args) {
+        System.out.println("===========================工厂模式===========================");
         Vehicle vehicle = null;
         // 1 Bicycle, 2 Car
         vehicle = VehicleFactory.createVehicle(1);
         vehicle.countWheel();
-        System.out.println("===============================================");
+        System.out.println("=============================================================");
         vehicle = VehicleFactory.createVehicle(2);
         vehicle.countWheel();
+        System.out.println("===========================工厂模式===========================");
     }
 }
